95b0302363828f41d03620654e00058204623c47
Michael Biebl <mbiebl@messageconcept.com>
2018-10-18 20:44:43 +0200

    Remove unused entries from navigation drawer
    We don't provide any of those facilities yet, so remove them for now.

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/DefaultAccountsDrawerHandler.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/DefaultAccountsDrawerHandler.kt
@@ -26,8 +26,6 @@ class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
     override fun initMenu(context: Context, menu: Menu) {
         if (BuildConfig.VERSION_NAME.contains("-alpha") || BuildConfig.VERSION_NAME.contains("-beta") || BuildConfig.VERSION_NAME.contains("-rc"))
             menu.findItem(R.id.nav_beta_feedback).isVisible = true
-        if (/* ose */ true)
-            menu.findItem(R.id.nav_donate).isVisible = true
     }
 
     override fun onNavigationItemSelected(activity: Activity, item: MenuItem): Boolean {
@@ -40,32 +38,9 @@ class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
             R.id.nav_app_settings ->
                 activity.startActivity(Intent(activity, AppSettingsActivity::class.java))
 
-            R.id.nav_twitter ->
-                UiUtils.launchUri(activity,
-                        Uri.parse("https://twitter.com/" + activity.getString(R.string.twitter_handle)))
             R.id.nav_website ->
                 UiUtils.launchUri(activity,
                         App.homepageUrl(activity))
-
-            R.id.nav_webdav_mounts ->
-                activity.startActivity(Intent(activity, WebdavMountsActivity::class.java))
-
-            R.id.nav_manual ->
-                UiUtils.launchUri(activity,
-                        App.homepageUrl(activity).buildUpon().appendPath("manual").build())
-            R.id.nav_faq ->
-                UiUtils.launchUri(activity,
-                        App.homepageUrl(activity).buildUpon().appendPath("faq").build())
-            R.id.nav_forums ->
-                UiUtils.launchUri(activity,
-                        App.homepageUrl(activity).buildUpon().appendPath("forums").build())
-            R.id.nav_donate ->
-                //if (BuildConfig.FLAVOR != App.FLAVOR_GOOGLE_PLAY)
-                    UiUtils.launchUri(activity,
-                            App.homepageUrl(activity).buildUpon().appendPath("donate").build())
-            R.id.nav_privacy ->
-                UiUtils.launchUri(activity,
-                        App.homepageUrl(activity).buildUpon().appendPath("privacy").build())
 
             else ->
                 return false
