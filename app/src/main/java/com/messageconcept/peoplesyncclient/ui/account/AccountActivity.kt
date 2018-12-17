d431d92bff67ff3293f74187f3e40bdf57b5878c
Michael Biebl <mbiebl@messageconcept.com>
2018-12-17 15:41:31 +0100

    AccountActivity: Trigger collection detection when account is openend
    Run the collection refresh in onLoadFinished() so we have the account
    info available. As a collection detection does itself trigger
    onLoadFinished(), make sure we don't end up in an endless loop by
    storing the state in a "refreshed" variable.

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/account/AccountActivity.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/account/AccountActivity.kt
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.lifecycle.*
+import com.messageconcept.peoplesyncclient.DavService
 import com.messageconcept.peoplesyncclient.DavUtils
 import com.messageconcept.peoplesyncclient.R
 import com.messageconcept.peoplesyncclient.databinding.ActivityAccountBinding
@@ -43,6 +44,7 @@ class AccountActivity: AppCompatActivity() {
                 ?: throw IllegalArgumentException("AccountActivity requires EXTRA_ACCOUNT")
         Model.Factory(application, account)
     }
+    private var refreshed = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,6 +67,16 @@ class AccountActivity: AppCompatActivity() {
         binding.viewPager.adapter = tabsAdapter
         model.cardDavService.observe(this, Observer {
             tabsAdapter.cardDavSvcId = it
+            if (!refreshed) {
+                Logger.log.info("Refreshing CardDAV collections")
+                val intent = Intent(this, DavService::class.java)
+                intent.action = DavService.ACTION_REFRESH_COLLECTIONS
+                intent.putExtra(DavService.EXTRA_DAV_SERVICE_ID, it)
+                startService(intent)
+
+                refreshed = true
+            }
+
         })
 
         binding.sync.setOnClickListener {
