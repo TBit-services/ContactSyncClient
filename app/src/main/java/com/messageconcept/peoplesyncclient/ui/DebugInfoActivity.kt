messageconcept/PeopleSyncClient

Fix debug info report file name

app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
@@ -86,7 +86,7 @@ class DebugInfoActivity: AppCompatActivity(), LoaderManager.LoaderCallbacks<Stri
                 if (!(debugInfoDir.exists() && debugInfoDir.isDirectory) && !debugInfoDir.mkdir())
                     throw IOException("Couldn't create debug directory")
 
-                val reportFile = File(debugInfoDir, "davx5-info.txt")
+                val reportFile = File(debugInfoDir, "peoplesync-info.txt")
                 Logger.log.fine("Writing debug info to ${reportFile.absolutePath}")
                 val writer = FileWriter(reportFile)
                 writer.write(it)

TBit-services/ContactSyncClient

app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
@@ -482,7 +482,7 @@ class DebugInfoActivity: AppCompatActivity() {
                 try {
                     zipProgress.postValue(true)
 
-                    val zipFile = File(debugInfoDir, "davx5-debug.zip")
+                    val zipFile = File(debugInfoDir, "peoplesync-debug.zip")
                     Logger.log.fine("Writing debug info to ${zipFile.absolutePath}")
                     ZipOutputStream(zipFile.outputStream().buffered()).use { zip ->
                         zip.setLevel(9)