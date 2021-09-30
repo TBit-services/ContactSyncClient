messageconcept/PeopleSyncClient

Show user_name and base_url user-data in debug info

app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
@@ -44,6 +44,8 @@ import com.messageconcept.peoplesyncclient.log.Logger
 import com.messageconcept.peoplesyncclient.model.AppDatabase
 import com.messageconcept.peoplesyncclient.resource.LocalAddressBook
 import com.messageconcept.peoplesyncclient.settings.AccountSettings
+import com.messageconcept.peoplesyncclient.settings.AccountSettings.Companion.KEY_BASE_URL
+import com.messageconcept.peoplesyncclient.settings.AccountSettings.Companion.KEY_USERNAME
 import org.apache.commons.lang3.exception.ExceptionUtils
 import java.io.File
 import java.io.FileWriter
@@ -266,6 +268,12 @@ class DebugInfoActivity: AppCompatActivity() {
                         }
                         text.append("\n  [CardDAV] Contact group method: ${accountSettings.getGroupMethod()}")
                                 .append("\n")
+                        accountManager.getUserData(acct, KEY_USERNAME)?.let { userName ->
+                            text.append("  Username: ${userName}\n")
+                        }
+                        accountManager.getUserData(acct, KEY_BASE_URL)?.let { baseUrl ->
+                            text.append("  Base URL: ${baseUrl}\n")
+                        }
                     } catch (e: InvalidAccountException) {
                         text.append("$acct is invalid (unsupported settings version) or does not exist\n")
                     }

TBit-services/ContactSyncClient

app/src/main/java/com/messageconcept/peoplesyncclient/ui/DebugInfoActivity.kt
@@ -50,6 +50,8 @@ import com.messageconcept.peoplesyncclient.log.Logger
 import com.messageconcept.peoplesyncclient.model.AppDatabase
 import com.messageconcept.peoplesyncclient.resource.LocalAddressBook
 import com.messageconcept.peoplesyncclient.settings.AccountSettings
+import com.messageconcept.peoplesyncclient.settings.AccountSettings.Companion.KEY_BASE_URL
+import com.messageconcept.peoplesyncclient.settings.AccountSettings.Companion.KEY_USERNAME
 import com.messageconcept.peoplesyncclient.settings.SettingsManager
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -432,7 +434,7 @@ class DebugInfoActivity: AppCompatActivity() {
                 val mainAccounts = accountManager.getAccountsByType(context.getString(R.string.account_type))
                 val addressBookAccounts = accountManager.getAccountsByType(context.getString(R.string.account_type_address_book)).toMutableList()
                 for (account in mainAccounts) {
-                    dumpMainAccount(account, writer)
+                    dumpMainAccount(account, accountManager, writer)
 
                     val iter = addressBookAccounts.iterator()
                     while (iter.hasNext()) {
@@ -520,7 +522,7 @@ class DebugInfoActivity: AppCompatActivity() {
         }
 
 
-        private fun dumpMainAccount(account: Account, writer: Writer) {
+        private fun dumpMainAccount(account: Account, accountManager: AccountManager, writer: Writer) {
             val context = getApplication<Application>()
 
             writer.append(" - Account: ${account.name}\n")
@@ -532,6 +534,12 @@ class DebugInfoActivity: AppCompatActivity() {
                     writer.append(", SSIDs: ${ssids.joinToString(", ")}")
                 }
                 writer.append("\n  Contact group method: ${accountSettings.getGroupMethod()}\n")
+                accountManager.getUserData(account, KEY_USERNAME)?.let { userName ->
+                    writer.append("  Username: ${userName}\n")
+                }
+                accountManager.getUserData(account, KEY_BASE_URL)?.let { baseUrl ->
+                    writer.append("  Base URL: ${baseUrl}\n")
+                }
             } catch(e: InvalidAccountException) {
                 writer.append("$e\n")
             }