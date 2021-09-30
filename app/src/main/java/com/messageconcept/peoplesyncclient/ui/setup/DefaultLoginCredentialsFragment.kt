messageconcept/PeopleSyncClient

Enforce restrictions during account setup

app/src/main/java/com/messageconcept/peoplesyncclient/ui/setup/DefaultLoginCredentialsFragment.kt
@@ -8,7 +8,9 @@
 
 package com.messageconcept.peoplesyncclient.ui.setup
 
+import android.content.Context
 import android.content.Intent
+import android.content.RestrictionsManager
 import android.net.MailTo
 import android.os.Bundle
 import android.os.Handler
@@ -27,6 +29,11 @@ import java.net.URISyntaxException
 
 class DefaultLoginCredentialsFragment: Fragment() {
 
+    companion object {
+        private const val KEY_LOGIN_BASE_URL = "login_base_url"
+        private const val KEY_LOGIN_USER_NAME = "login_user_name"
+    }
+
     private lateinit var model: DefaultLoginCredentialsModel
     private lateinit var loginModel: LoginModel
 
@@ -41,6 +48,21 @@ class DefaultLoginCredentialsFragment: Fragment() {
         // initialize model on first call
         if (savedInstanceState == null)
             activity?.intent?.let { model.initialize(it) }
+
+
+        val restrictionsManager = activity?.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
+        val appRestrictions = restrictionsManager.applicationRestrictions
+        var entries = restrictionsManager.getManifestRestrictions(activity?.applicationContext?.packageName)
+
+        if (appRestrictions.containsKey(KEY_LOGIN_BASE_URL)) {
+            model.baseUrl.value = appRestrictions.getString(KEY_LOGIN_BASE_URL)
+            model.loginWithEmailAddress.value = false
+            model.loginWithUrlAndCertificate.value = false
+            model.loginWithUrlAndUsername.value = true
+        }
+        if (appRestrictions.containsKey(KEY_LOGIN_USER_NAME)) {
+            model.username.value = appRestrictions.getString(KEY_LOGIN_USER_NAME)
+        }
 
         v.selectCertificate.setOnClickListener {
             KeyChain.choosePrivateKeyAlias(requireActivity(), { alias ->

TBit-services/ContactSyncClient

app/src/main/java/com/messageconcept/peoplesyncclient/ui/setup/DefaultLoginCredentialsFragment.kt
@@ -8,7 +8,9 @@
 
 package com.messageconcept.peoplesyncclient.ui.setup
 
+import android.content.Context
 import android.content.Intent
+import android.content.RestrictionsManager
 import android.net.MailTo
 import android.os.Bundle
 import android.os.Handler
@@ -29,6 +31,11 @@ import java.net.URISyntaxException
 
 class DefaultLoginCredentialsFragment : Fragment() {
 
+    companion object {
+        private const val KEY_LOGIN_BASE_URL = "login_base_url"
+        private const val KEY_LOGIN_USER_NAME = "login_user_name"
+    }
+
     val loginModel by activityViewModels<LoginModel>()
     val model by viewModels<DefaultLoginCredentialsModel>()
 
@@ -41,6 +48,20 @@ class DefaultLoginCredentialsFragment : Fragment() {
         // initialize model on first call
         if (savedInstanceState == null)
             activity?.intent?.let { model.initialize(it) }
+
+        val restrictionsManager = activity?.getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager
+        val appRestrictions = restrictionsManager.applicationRestrictions
+        var entries = restrictionsManager.getManifestRestrictions(activity?.applicationContext?.packageName)
+
+        if (appRestrictions.containsKey(KEY_LOGIN_BASE_URL)) {
+            model.baseUrl.value = appRestrictions.getString(KEY_LOGIN_BASE_URL)
+            model.loginAdvanced.value = false
+            model.loginWithEmailAddress.value = false
+            model.loginWithUrlAndUsername.value = true
+        }
+        if (appRestrictions.containsKey(KEY_LOGIN_USER_NAME)) {
+            model.username.value = appRestrictions.getString(KEY_LOGIN_USER_NAME)
+        }
 
         v.loginUrlBaseUrlEdittext.setAdapter(DefaultLoginCredentialsModel.LoginUrlAdapter(requireActivity()))
 