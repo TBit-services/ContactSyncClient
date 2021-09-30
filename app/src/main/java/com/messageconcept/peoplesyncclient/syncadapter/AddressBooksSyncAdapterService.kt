messageconcept/PeopleSyncClient

Defer cleanup of old addressbook accounts
Wait with deleting old addressbook accounts (and the associated
contacts) until at least 1 new address book account has been added.

While this doesn't guarantee that all contacts have been resynced, it at
least means that we were able to successfully contact the server
It also has the benefit of being a rather non-invasive change.

app/src/main/java/com/messageconcept/peoplesyncclient/syncadapter/AddressBooksSyncAdapterService.kt
@@ -152,6 +152,12 @@ class AddressBooksSyncAdapterService : SyncAdapterService() {
                         Logger.log.log(Level.INFO, "Adding local address book", info)
                         LocalAddressBook.create(context, contactsProvider, account, info)
                     }
+                    // trigger cleanup of old accounts now that we discovered new address books
+                    // TODO: only trigger if there are actually still old address book accounts?
+                    if (remote.isNotEmpty()) {
+                        Logger.log.info("New address books found, triggering cleanup of old accounts")
+                        AccountAuthenticatorService.cleanupAccounts(context);
+                    }
                 } finally {
                     @Suppress("DEPRECATION")
                     if (Build.VERSION.SDK_INT >= 24)

TBit-services/ContactSyncClient

app/src/main/java/com/messageconcept/peoplesyncclient/syncadapter/AddressBooksSyncAdapterService.kt
@@ -122,6 +122,12 @@ class AddressBooksSyncAdapterService : SyncAdapterService() {
                     Logger.log.log(Level.INFO, "Adding local address book", info)
                     LocalAddressBook.create(context, contactsProvider, account, info)
                 }
+                // trigger cleanup of old accounts now that we discovered new address books
+                // TODO: only trigger if there are actually still old address book accounts?
+                if (remoteAddressBooks.isNotEmpty()) {
+                    Logger.log.info("New address books found, triggering cleanup of old accounts")
+                    AccountUtils.cleanupAccounts(context);
+                }
             } finally {
                 contactsProvider?.closeCompat()
             }