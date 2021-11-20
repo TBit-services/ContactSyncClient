b6b75921b7d53f7b976275f76a79603b3e24cd4d
Michael Biebl <mbiebl@messageconcept.com>
2021-11-20 03:19:40 +0100

    Drop WebDAV related code and resources

--- app/src/main/java/com/messageconcept/peoplesyncclient/ui/DefaultAccountsDrawerHandler.kt
+++ app/src/main/java/com/messageconcept/peoplesyncclient/ui/DefaultAccountsDrawerHandler.kt
@@ -14,7 +14,6 @@ import android.widget.Toast
 import com.messageconcept.peoplesyncclient.App
 import com.messageconcept.peoplesyncclient.BuildConfig
 import com.messageconcept.peoplesyncclient.R
-import com.messageconcept.peoplesyncclient.ui.webdav.WebdavMountsActivity
 
 class DefaultAccountsDrawerHandler: IAccountsDrawerHandler {
 
