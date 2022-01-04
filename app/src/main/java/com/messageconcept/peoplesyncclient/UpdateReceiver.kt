messageconcept/PeopleSyncClient

Trigger a notification after the app has been upgraded
After migrating old app account settings, show a notification
informing the user about the upgrade.
Ask the user to tap the notification which will start the main activity
which in turn will trigger the battery optimization dialogs.

app/src/main/java/com/messageconcept/peoplesyncclient/UpdateReceiver.kt
@@ -10,21 +10,34 @@ package com.messageconcept.peoplesyncclient
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.app.PendingIntent
 import android.content.*
 import android.database.sqlite.SQLiteDatabase
 import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import at.bitfire.vcard4android.GroupMethod
 import com.messageconcept.peoplesyncclient.log.Logger
 import com.messageconcept.peoplesyncclient.model.Credentials
 import com.messageconcept.peoplesyncclient.model.ServiceDB
 import com.messageconcept.peoplesyncclient.settings.AccountSettings
+import com.messageconcept.peoplesyncclient.ui.AccountsActivity
+import com.messageconcept.peoplesyncclient.ui.NotificationUtils
 import java.util.logging.Level
 
 class UpdateReceiver : BroadcastReceiver() {
+    companion object {
+        const val KEY_USERNAME = "user_name"
+        const val KEY_PROVIDED_URL = "provided_url"
+        const val KEY_PARENT_ACCOUNT_NAME = "parent_account_name"
+        const val KEY_ADDRESSBOOK_URL = "addressbook_url"
+    }
 
     override fun onReceive(context: Context, intent: Intent?) {
         Logger.log.fine("App got updated")
         val accountManager = AccountManager.get(context)
+        var showNotification = false
+
 
         for (account in accountManager.getAccountsByType(context.getString(R.string.account_type))) {
             val versionStr = accountManager.getUserData(account, AccountSettings.KEY_SETTINGS_VERSION)
@@ -34,8 +47,8 @@ class UpdateReceiver : BroadcastReceiver() {
                 val newName = account.name.removePrefix("PeopleSync ")
                 val newAccount = Account(newName, context.getString(R.string.account_type))
 
-                val userName = accountManager.getUserData(account, "user_name")
-                val providedURL = accountManager.getUserData(account, "provided_url")
+                val userName = accountManager.getUserData(account, KEY_USERNAME)
+                val providedURL = accountManager.getUserData(account, KEY_PROVIDED_URL)
                 val password = accountManager.getPassword(account)
 
                 val credentials = Credentials(userName, password)
@@ -48,6 +61,8 @@ class UpdateReceiver : BroadcastReceiver() {
                     // TODO: Exit at this point or continue?
                 }
                 delete(context, account)
+
+                showNotification = true
 
                 // add entries for account to service DB
                 ServiceDB.OpenHelper(context).use { dbHelper ->
@@ -92,6 +107,25 @@ class UpdateReceiver : BroadcastReceiver() {
                 Logger.log.info("Removing old account ${account.name}")
             }
         }
+
+        if (showNotification) {
+            val appIntent = Intent(context, AccountsActivity::class.java)
+
+            val notify = NotificationUtils.newBuilder(context, NotificationUtils.CHANNEL_DEBUG)
+                    .setSmallIcon(R.drawable.ic_settings_action)
+                    .setContentTitle(context.getString(R.string.update_receiver_notify))
+                    .setContentText(context.getString(R.string.update_receiver_notify_action))
+                    .setContentIntent(PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT))
+                    .setSubText("app update")
+                    .setOnlyAlertOnce(false)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_STATUS)
+                    .setAutoCancel(true)
+                    .build()
+            NotificationManagerCompat.from(context)
+                    .notify(null, NotificationUtils.NOTIFY_UPDATE, notify)
+        }
+
     }
 
     fun delete(context: Context, account: Account) {

TBit-services/ContactSyncClient

app/src/main/java/com/messageconcept/peoplesyncclient/UpdateReceiver.kt
@@ -10,8 +10,11 @@ package com.messageconcept.peoplesyncclient
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.app.PendingIntent
 import android.content.*
 import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import at.bitfire.vcard4android.GroupMethod
 import com.messageconcept.peoplesyncclient.log.Logger
 import com.messageconcept.peoplesyncclient.model.AppDatabase
@@ -20,15 +23,24 @@ import com.messageconcept.peoplesyncclient.model.Service
 import com.messageconcept.peoplesyncclient.settings.AccountSettings
 import com.messageconcept.peoplesyncclient.settings.Settings
 import com.messageconcept.peoplesyncclient.settings.SettingsManager
+import com.messageconcept.peoplesyncclient.ui.AccountsActivity
+import com.messageconcept.peoplesyncclient.ui.NotificationUtils
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import java.util.logging.Level
 import kotlin.concurrent.thread
 
 class UpdateReceiver : BroadcastReceiver() {
+    companion object {
+        const val KEY_USERNAME = "user_name"
+        const val KEY_PROVIDED_URL = "provided_url"
+        const val KEY_PARENT_ACCOUNT_NAME = "parent_account_name"
+        const val KEY_ADDRESSBOOK_URL = "addressbook_url"
+    }
 
     override fun onReceive(context: Context, intent: Intent?) {
         Logger.log.fine("App got updated")
         val accountManager = AccountManager.get(context)
+        var showNotification = false
 
         for (account in accountManager.getAccountsByType(context.getString(R.string.account_type))) {
             val versionStr = accountManager.getUserData(account, AccountSettings.KEY_SETTINGS_VERSION)
@@ -38,8 +50,8 @@ class UpdateReceiver : BroadcastReceiver() {
                 val newName = account.name.removePrefix("PeopleSync ")
                 val newAccount = Account(newName, context.getString(R.string.account_type))
 
-                val userName = accountManager.getUserData(account, "user_name")
-                val providedURL = accountManager.getUserData(account, "provided_url")
+                val userName = accountManager.getUserData(account, KEY_USERNAME)
+                val providedURL = accountManager.getUserData(account, KEY_PROVIDED_URL)
                 val password = accountManager.getPassword(account)
 
                 val credentials = Credentials(userName, password)
@@ -52,6 +64,8 @@ class UpdateReceiver : BroadcastReceiver() {
                     // TODO: Exit at this point or continue?
                 }
                 delete(context, account)
+
+                showNotification = true
 
                 thread {
                     // add entries for account to service DB
@@ -97,6 +111,24 @@ class UpdateReceiver : BroadcastReceiver() {
                 Logger.log.info("Removing old account ${account.name}")
             }
         }
+
+        if (showNotification) {
+            val appIntent = Intent(context, AccountsActivity::class.java)
+
+            val notify = NotificationUtils.newBuilder(context, NotificationUtils.CHANNEL_DEBUG)
+                    .setSmallIcon(R.drawable.ic_settings)
+                    .setContentTitle(context.getString(R.string.update_receiver_notify))
+                    .setContentText(context.getString(R.string.update_receiver_notify_action))
+                    .setContentIntent(PendingIntent.getActivity(context, 0, appIntent, PendingIntent.FLAG_UPDATE_CURRENT))
+                    .setOnlyAlertOnce(false)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_STATUS)
+                    .setAutoCancel(true)
+                    .build()
+            NotificationManagerCompat.from(context)
+                    .notify(null, NotificationUtils.NOTIFY_UPDATE, notify)
+        }
+
     }
 
     fun delete(context: Context, account: Account) {